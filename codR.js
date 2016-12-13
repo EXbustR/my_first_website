7bc5d9c38ce5b846345426d4ca64c695e357ad4a
jose <jalcaraz007@gmail.com>
2016-12-13 13:38:50 -0800

    added animation on the header

--- codR.js
+++ codR.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-	$('.container').fadeIn(3000);
+	$('.container').fadeIn(3000)
+	$('.container').slideDown(3000);
 });
+
+
+$(document).ready(function(){
+        var div = $(".animate");  
+        div.animate({fontSize:'1em'}, 5000);
+    });
